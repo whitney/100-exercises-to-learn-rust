65d983a4179d3797facb4e50c13d98e30744c376
Whitney Zoller <whitneyrzoller@gmail.com>
2024-12-06 00:24:35 -0500

    wip

--- exercises/02_basic_calculator/01_integers/src/lib.rs
+++ exercises/02_basic_calculator/01_integers/src/lib.rs
@@ -1,6 +1,6 @@
 fn compute(a: u32, b: u32) -> u32 {
     // TODO: change the line below to fix the compiler error and make the tests pass.
-    a + b * a + b
+    (a + b) * (a + b)
 }
 
 #[cfg(test)]
--- exercises/02_basic_calculator/10_as_casting/src/lib.rs
+++ exercises/02_basic_calculator/10_as_casting/src/lib.rs
@@ -6,7 +6,7 @@ mod tests {
 
     #[test]
     fn u16_to_u32() {
-        let v: u32 = todo!();
+        let v: u32 = 47;
         assert_eq!(47u16 as u32, v);
     }
 
@@ -24,14 +24,15 @@ mod tests {
         // You could solve this by using exactly the same expression as above,
         // but that would defeat the purpose of the exercise. Instead, use a genuine
         // `i8` value that is equivalent to `255` when converted to `u8`.
-        let y: i8 = todo!();
+        // NOTE: the u32 binary for 255 is 00000000 00000000 00000000 11111111
+        let y: i8 = -1;
 
         assert_eq!(x, y);
     }
 
     #[test]
     fn bool_to_u8() {
-        let v: u8 = todo!();
+        let v: u8 = 1;
         assert_eq!(true as u8, v);
     }
 }
